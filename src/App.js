97e167b6b1f46aec418ae2f115871b119f7842fc
Cheolkie Kim <ckiekim@naver.com>
2020-02-27 21:55:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Clock from './Components/Clock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date()
+		};
+	}
+	goBack() {
+		let nextDate = this.state.date;
+		nextDate.setSeconds(nextDate.getSeconds() - 10);
+		this.setState({
+			date: nextDate
+		});
+	}
+	render() {
+		return (
+			<div className="App">			
+				<h3>현재시각: {this.state.date.toLocaleTimeString()}</h3>
+				<button onClick={this.goBack.bind(this)}>10초 뒤로가기</button>
+				<Clock/>
+			</div>
+		);
+	}
 }
 
 export default App;
